eb17f4ed792f6007a368b4c4da59cd63fbd6d400
tnkTaka <taka.abc.hiko@gmail.com>
2018-11-17 19:54:35 +0900

    init

---
+++ app/src/main/java/local/hal/st32/android/itarticlecollection60213/ArticleAddActivity.java
@@ -0,0 +1,13 @@
+package local.hal.st32.android.itarticlecollection60213;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ArticleAddActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_article_add);
+    }
+}
